19b9e1c963921be2a8b5bb01a430ddf7603e8548
Rugved Kharde <rugvedkharde2211@gmail.com>
2025-11-02 21:24:57 +0530

    Add Chip import back to CourseDetailPage and remove unused import

--- frontend/src/pages/CourseDetailPage.tsx
+++ frontend/src/pages/CourseDetailPage.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Box, Typography, Container, Button, Card, CardContent, Rating, Stack, TextField, Dialog, DialogTitle, DialogContent, DialogActions, Alert, MenuItem, Select, FormControl, InputLabel, IconButton, LinearProgress } from '@mui/material';
+import { Box, Typography, Container, Button, Card, CardContent, Rating, Stack, Chip, TextField, Dialog, DialogTitle, DialogContent, DialogActions, Alert, MenuItem, Select, FormControl, InputLabel, IconButton, LinearProgress } from '@mui/material';
 import { useParams, useNavigate } from 'react-router-dom';
-import { fetchCourseById, fetchCourseRatingSummary, fetchContentByCourse, logContentAccess, fetchCourseReviews, createReview, enrollInCourse, getStudentEnrollments, ContentItem, addContentItem, deleteContentItem, updateEnrollmentProgress, updateEnrollmentCompletion, getCourseEnrollments, updateEnrollmentStage1, updateEnrollmentStage2, updateEnrollmentCurrentStage } from '../services/api';
+import { fetchCourseById, fetchCourseRatingSummary, fetchContentByCourse, logContentAccess, fetchCourseReviews, createReview, enrollInCourse, getStudentEnrollments, ContentItem, addContentItem, deleteContentItem, updateEnrollmentProgress, updateEnrollmentCompletion, getCourseEnrollments, updateEnrollmentStage1, updateEnrollmentStage2 } from '../services/api';
 import { useAuth } from '../contexts/AuthContext';
 import DeleteIcon from '@mui/icons-material/Delete';
 import AddIcon from '@mui/icons-material/Add';
